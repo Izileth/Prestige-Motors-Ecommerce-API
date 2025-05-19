1f342eb74f035fbaa0b44f926bcd4c0064282319
Kawã Kleber <kawaklebersc@gmail.com>
2025-05-19 20:55:21 -0300

    Update Cors Definitions

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ app.use(morgan('combined'));
 // 2. CORS Dinâmico
 const allowedOrigins = [
     process.env.FRONTEND_URL,
-    'http://localhost:5173'
+    'https://vortex-motors-services.vercel.app'
 ].filter(Boolean);
 
 app.use(cors({
@@ -29,7 +29,7 @@ app.use(cors({
     },
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Cache-Control'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Cache-Control', 'Access-Control-Allow-Origin'],
     exposedHeaders: ['Set-Cookie']
 }));
 
